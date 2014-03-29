d5355e9dd269bb4b7cee41bdd5a31a7dd148704f
unknown <derek.beattie@gmail.com>
2014-03-29 10:23:51 -0500

    blah

--- DemoApp/src/DemoApp.Phone/Views/FirstView.xaml.cs
+++ DemoApp/src/DemoApp.Phone/Views/FirstView.xaml.cs
@@ -1,6 +1,8 @@
+using Cirrious.MvvmCross.WindowsPhone.Views;
+
 namespace DemoApp.Phone.Views
 {
-    public partial class FirstView
+    public partial class FirstView : MvxPhonePage
     {
         public FirstView()
         {
